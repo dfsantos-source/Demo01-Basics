5595d2605530872414ccf1dc18969ebbb39f490e
Dane Santos <64881219+dfsantos-source@users.noreply.github.com>
2020-12-21 21:02:36 -0500

    added spin component

--- Assets/SpinComponent.cs
+++ Assets/SpinComponent.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class SpinComponent : MonoBehaviour
 {
+    // variable if the message is printed or not
     private bool printedMessage;
+
+    // variable for Transform object
+    private Transform myTransform;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Hello from Start");
+        myTransform = GetComponent<Transform>();
+        Debug.Log(myTransform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        // rotates Transform object 
+        myTransform.Rotate(0.1f,0f,0f);
+        
+
         if (!printedMessage) 
         {
             Debug.Log("Hello from Update");
